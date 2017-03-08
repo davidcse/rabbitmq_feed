7d6bd439466530d76d77b79deed58c545dffbab0
David Lin <david.lin.cse@gmail.com>
2017-03-07 20:36:05 -0500

    get test

--- exchangeServer.js
+++ exchangeServer.js
@@ -83,4 +83,4 @@ var server = app.listen(80, function () {
   var host = server.address().address
   var port = server.address().port
   console.log("RabbitMQ server is listening at http://%s:%s", host, port)
-});
+})
