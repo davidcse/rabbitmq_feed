7363d4b0df800d43d0a575474ce0e24cc283a350
David Lin <david.lin.cse@gmail.com>
2017-03-07 20:27:57 -0500

    get check

--- exchangeServer.js
+++ exchangeServer.js
@@ -47,6 +47,14 @@ app.post('/listen', function (req, res) {
 
 });
 
+app.get('/speak',function(req,res){
+  console.log("speak GET works");
+  res.end({"status":"OK","message":"Please use POST request instead"});
+})
+app.get('/listen',function(req,res){
+  console.log("lisen GET works");
+  res.end({"status":"OK","message":"Please use POST request instead"});
+})
 
 app.post('/speak',function(req,res){
   if(!req.body.key || !req.body.msg){
