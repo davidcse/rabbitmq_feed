c0b010e095d9e358427cc7c96e0e89ef778a6b21
David Lin <david.lin.cse@gmail.com>
2017-03-07 23:41:52 -0500

    change to binding

--- exchangeServer.js
+++ exchangeServer.js
@@ -17,7 +17,6 @@ var ex = 'hw3';
 /************************************/
 /*  REST ENDPOINTS                  */
 /***********************************/
-
 app.post('/listen', function (req, res) {
   console.log("POST /listen : " + JSON.stringify(req.body));
   var keys = [];
@@ -28,25 +27,17 @@ app.post('/listen', function (req, res) {
   amqp.connect('amqp://localhost', function(err, conn) {
     conn.createChannel(function(err, ch) {
       ch.assertExchange(ex, 'direct', {durable: false});
-      for(var i=0; i< keys.length; i++){
-        ch.assertQueue(keys[i], {exclusive: true}, function(err, q) {
+        ch.assertQueue('', {exclusive: true}, function(err, q) {
           console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
-          console.log("q.queue, q is : " + JSON.stringify(q));
-          ch.bindQueue(q.queue, ex, function(err, q){
-            if(err){
-              ch.unbindQueue(q.queue, ex, function(err,q){
-                return res.json({"status":"ERROR","msg":"Failed binding to the queue"});
-              });
-              return
-            }
-            console.log("bound to queue : %s",q.queue);
-            ch.consume(q.queue, function(msg) {
-              console.log(" [x] %s", msg.content.toString());
-              res.json({"msg":msg.content.toString()});
-            }, {noAck: true});
-          });
+          for(var i=0;i<keys.length;i++){
+            ch.bindQueue(q.queue, ex, keys[i]);
+          }
+          ch.consume(q.queue, function(msg) {
+            console.log(" [x] %s", msg.content.toString());
+            return res.json({"msg":msg.content.toString()});
+          }, {noAck: true});
+
         });
-      }
     });
     //setTimeout(function() { conn.close(); }, 500);
     console.log("Finished /listen");
@@ -55,14 +46,6 @@ app.post('/listen', function (req, res) {
 
 });
 
-app.get('/speak',function(req,res){
-  console.log("speak GET works");
-  res.json({"status":"OK","message":"Please use POST request instead"});
-})
-app.get('/listen',function(req,res){
-  console.log("lisen GET works");
-  res.json({"status":"OK","message":"Please use POST request instead"});
-})
 
 app.post('/speak',function(req,res){
   if(!req.body.key || !req.body.msg){
@@ -81,9 +64,74 @@ app.post('/speak',function(req,res){
       console.log(" [x] Sent msg:%s    key: %s", msg, key);
       res.json({"status":"OK"});
     });
+    setTimeout(function() { conn.close()}, 500);
   });
   console.log("Finished /speak");
 });
+
+
+app.get('/speak',function(req,res){
+  console.log("speak works");
+  console.log("_______________________________________________");
+  var amqp = require('amqplib/callback_api');
+
+  amqp.connect('amqp://localhost', function(err, conn) {
+    conn.createChannel(function(err, ch) {
+      var ex = 'logs2';
+      var msg = 'Hello World!';
+      ch.assertExchange(ex, 'direct', {durable: false});
+      ch.publish(ex, "mr.muffin", new Buffer(msg));
+      console.log(" [x] Published to exchange: %s , message: %s", ex,msg);
+      res.json({"status":"OK","message":"Finished Producing"});
+    });
+    console.log("Timeout!!!!!!!!\n");
+    setTimeout(function() { conn.close();}, 500);
+  });
+});
+
+
+app.get('/listen',function(req,res){
+  console.log("listen works");
+  console.log("_______________________________________________");
+  var amqp = require('amqplib/callback_api');
+  var ex = 'logs2';
+
+  amqp.connect('amqp://localhost', function(err, conn) {
+    conn.createChannel(function(err, ch) {
+      ch.assertExchange(ex, 'direct', {durable: false});
+      try{
+        ch.assertQueue('myqueue', {exclusive: true}, function(err, q) {
+          if(err){
+            console.log("first try block error: "+err);
+          }
+          console.log('Asserted Queue : %s',q.queue);
+          ch.bindQueue(q.queue, ex, 'mr.muffin');
+          ch.consume(q.queue, function(msg) {
+            console.log(" [x] Finished Consuming string : %s", msg.content.toString());
+            res.json({"status":"OK","message":"Finished Consuming"});
+          }, {noAck: true});
+          res.json({"status":"error took too long"});
+        });
+      }catch(err){
+        console.log("Caught an ERROR when asserting the queue");
+        try{
+          ch.consume('myqueue', function(msg) {
+            console.log(" [x] Finished Consuming string : %s", msg.content.toString());
+            res.json({"status":"OK","message":"Finished Consuming"});
+          }, {noAck: true});
+          res.json({"status":"error took too long"});
+        }catch(err2){
+          console.log("caught ERROR2");
+
+        }
+
+      }
+    });
+  });
+});
+
+
+
 
 
 // Starts the server
