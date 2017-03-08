d518e074f50042278c812de0ba0d1a26144446f0
David Lin <david.lin.cse@gmail.com>
2017-03-07 23:57:58 -0500

    asdf

--- exchangeServer.js
+++ exchangeServer.js
@@ -50,7 +50,7 @@ app.post('/listen', function (req, res) {
 
 app.post('/speak',function(req,res){
   if(!req.body.key || !req.body.msg){
-    return
+    return res.json({"status":"error"});
   }
   console.log("POST /speak : " + JSON.stringify(req.body));
   //publish to the hw3 Exchange
@@ -63,78 +63,13 @@ app.post('/speak',function(req,res){
       //publish msg to exchange, using key.
       ch.publish(ex, key, Buffer.from(msg));
       console.log(" [x] Sent msg:%s    key: %s", msg, key);
-      res.json({"status":"OK"});
+      return res.json({"status":"OK"});
     });
-     //setTimeout(function() { conn.close(); res.json({"status":"timedout"})}, 500);
+    setTimeout(function() { conn.close(); return res.json({"status":"timedout"})}, 500);
   });
   console.log("Finished /speak");
 });
 
-
-/*
-
-app.get('/speak',function(req,res){
-  console.log("speak works");
-  console.log("_______________________________________________");
-  var amqp = require('amqplib/callback_api');
-
-  amqp.connect('amqp://localhost', function(err, conn) {
-    conn.createChannel(function(err, ch) {
-      var ex = 'logs2';
-      var msg = 'Hello World!';
-      ch.assertExchange(ex, 'direct', {durable: false});
-      ch.publish(ex, "mr.muffin", new Buffer(msg));
-      console.log(" [x] Published to exchange: %s , message: %s", ex,msg);
-      res.json({"status":"OK","message":"Finished Producing"});
-    });
-    console.log("Timeout!!!!!!!!\n");
-    setTimeout(function() { conn.close();}, 500);
-  });
-});
-
-
-app.get('/listen',function(req,res){
-  console.log("listen works");
-  console.log("_______________________________________________");
-  var amqp = require('amqplib/callback_api');
-  var ex = 'logs2';
-
-  amqp.connect('amqp://localhost', function(err, conn) {
-    conn.createChannel(function(err, ch) {
-      ch.assertExchange(ex, 'direct', {durable: false});
-      try{
-        ch.assertQueue('myqueue', {exclusive: true}, function(err, q) {
-          if(err){
-            console.log("first try block error: "+err);
-          }
-          console.log('Asserted Queue : %s',q.queue);
-          ch.bindQueue(q.queue, ex, 'mr.muffin');
-          ch.consume(q.queue, function(msg) {
-            console.log(" [x] Finished Consuming string : %s", msg.content.toString());
-            res.json({"status":"OK","message":"Finished Consuming"});
-          }, {noAck: true});
-          res.json({"status":"error took too long"});
-        });
-      }catch(err){
-        console.log("Caught an ERROR when asserting the queue");
-        try{
-          ch.consume('myqueue', function(msg) {
-            console.log(" [x] Finished Consuming string : %s", msg.content.toString());
-            res.json({"status":"OK","message":"Finished Consuming"});
-          }, {noAck: true});
-          res.json({"status":"error took too long"});
-        }catch(err2){
-          console.log("caught ERROR2");
-
-        }
-
-      }
-    });
-  });
-});
-
-
-*/
 
 
 // Starts the server
