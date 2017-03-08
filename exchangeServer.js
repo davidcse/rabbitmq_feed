1a2bd90374279e82abee3e378661a3cfce931741
David Lin <david.lin.cse@gmail.com>
2017-03-07 21:32:55 -0500

    fixed null in keys[i]

--- exchangeServer.js
+++ exchangeServer.js
@@ -30,16 +30,17 @@ app.post('/listen', function (req, res) {
       ch.assertExchange(ex, 'direct', {durable: false});
       for(var i=0; i< keys.length; i++){
         ch.assertQueue(keys[i], {exclusive: true}, function(err, q) {
-          console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", keys[i]);
-          ch.bindQueue(keys[i], ex, function(err, data){
+          console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+          console.log("q.queue, q is : " + JSON.stringify(q));
+          ch.bindQueue(q.queue, ex, function(err, q){
             if(err){
-              ch.unbindQueue(keys[i], ex, function(err,data){
+              ch.unbindQueue(q.queue, ex, function(err,q){
                 return res.json({"status":"ERROR","msg":"Failed binding to the queue"});
-              }])
+              });
               return
             }
             console.log("bound to queue : %s",q.queue);
-            ch.consume(keys[i], function(msg) {
+            ch.consume(q.queue, function(msg) {
               console.log(" [x] %s", msg.content.toString());
               res.json({"msg":msg.content.toString()});
             }, {noAck: true});
